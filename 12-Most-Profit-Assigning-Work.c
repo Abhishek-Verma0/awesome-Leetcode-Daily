34664ed04c6bfac80dfa21c5ae110405cc8ec7f7
Abhishek-Verma0 <144248104+Abhishek-Verma0@users.noreply.github.com>
2024-06-18 11:12:39 +0530

    Create 12-Most-Profit-Assigning-Work.c

---
+++ 12-Most-Profit-Assigning-Work.c
@@ -0,0 +1,55 @@
+/*
+You have n jobs and m workers. You are given three arrays: difficulty, profit, and worker where:
+
+    difficulty[i] and profit[i] are the difficulty and the profit of the ith job, and
+    worker[j] is the ability of jth worker (i.e., the jth worker can only complete a job with difficulty at most worker[j]).
+
+Every worker can be assigned at most one job, but one job can be completed multiple times.
+
+    For example, if three workers attempt the same job that pays $1, then the total profit will be $3. If a worker cannot complete any job, their profit is $0.
+
+Return the maximum profit we can achieve after assigning the workers to the jobs.
+
+ 
+
+Example 1:
+
+Input: difficulty = [2,4,6,8,10], profit = [10,20,30,40,50], worker = [4,5,6,7]
+Output: 100
+Explanation: Workers are assigned jobs of difficulty [4,4,6,6] and they get a profit of [20,20,30,30] separately.
+
+Example 2:
+
+Input: difficulty = [85,47,57], profit = [24,66,99], worker = [40,25,25]
+Output: 0
+
+*/
+
+
+
+
+int compare(const void* a,const void* b) {
+    int* inta = *(const int*)a;
+    int* intb = *(const int*)b;
+
+    return (inta>intb)-(intb>inta);
+}
+
+int maxProfitAssignment(int* difficulty, int difficultySize, int* profit, int profitSize, int* worker, int workerSize) {
+    qsort(worker,workerSize,sizeof(int),compare);
+
+    int total = 0;
+    for (int w = 0;w < workerSize;w++) {
+        int curworker = worker[w];
+        int index = 0;
+        int maxprofit = 0;
+        while (index < difficultySize){
+            if (difficulty[index]<=curworker && profit[index]>maxprofit) {
+                maxprofit = profit[index];
+            }
+            index++;
+        }
+        total+=maxprofit;
+    }
+    return total;
+}
