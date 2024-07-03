cb7da62a3824ddb70b5e7ac95cd1135b98c26de7
Abhishek-Verma0 <144248104+Abhishek-Verma0@users.noreply.github.com>
2024-07-03 08:19:29 +0530

    Update 34-SummaryRanges.c

--- 34-SummaryRanges.c
+++ 34-SummaryRanges.c
@@ -48,3 +48,30 @@ Constraints:
 
 
 
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+char ** summaryRanges(int* nums, int numsSize, int* returnSize){
+    int start = 0;
+    char** final_arr = (char**)malloc(20*sizeof(char*));
+    int count =0;
+        for (int k=0;k<numsSize;k++){
+            if (k==0 || nums[k]-1 > nums[k-1]){ // did "-1" here, since the number will not be stored (too large)
+                start=nums[k];
+            }
+            if (k==numsSize-1 || nums[k]+1 < nums[k+1]){ // we are at the finish line
+                // printf("[%d..%d]\n",start,nums[k]);
+                final_arr[count]= (char*) malloc(28*sizeof(char));
+                if (start<nums[k]){
+                    snprintf(final_arr[count],28,"%d->%d",start,nums[k]); 
+                }
+                else 
+                    snprintf(final_arr[count],28,"%d",start);
+                printf("%s\n",final_arr[count]);
+                count++;
+            }
+        }
+    *returnSize = count;
+    return final_arr;
+    
+}
