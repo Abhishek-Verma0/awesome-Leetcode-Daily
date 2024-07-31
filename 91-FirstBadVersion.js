04e3fffb4c5a7acfcbaace4e8b0e7f4236d8e4fa
Abhishek-Verma0 <144248104+Abhishek-Verma0@users.noreply.github.com>
2024-07-31 17:51:53 +0530

    solution change

--- 91-FirstBadVersion.js
+++ 91-FirstBadVersion.js
@@ -37,9 +37,38 @@ Constraints:
 // The API isBadVersion is defined for you.
 // bool isBadVersion(int version);
 
-int firstBadVersion(int n)
-{
-    if (!isBadVersion(n))
-        return n + 1;
-    return firstBadVersion(n - 1);
-}
+/**
+ * Definition for isBadVersion()
+ * 
+ * @param {integer} version number
+ * @return {boolean} whether the version is bad
+ * isBadVersion = function(version) {
+ *     ...
+ * };
+ */
+
+/**
+ * @param {function} isBadVersion()
+ * @return {function}
+ */
+var solution = function(isBadVersion) {
+    /**
+     * @param {integer} n Total versions
+     * @return {integer} The first bad version
+     */
+    return function(n) {
+        let start = 0;
+        let end = n;
+        let mid = Math.floor((start + end)/2);
+        while(start < end) {
+            if(isBadVersion(mid) && !isBadVersion(mid - 1)) return mid;
+            else if(!isBadVersion(mid) && isBadVersion(mid + 1)) return mid + 1;
+            else if(!isBadVersion(mid)) start = mid;
+            else end = mid;
+            mid = Math.floor((start + end)/2);
+        }
+    };
+};
+
+
+
