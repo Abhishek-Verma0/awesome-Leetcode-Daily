2096e9de2499bd3040e923f6e4dc73e290570a80
Abhishek-Verma0 <144248104+Abhishek-Verma0@users.noreply.github.com>
2024-08-12 17:44:33 +0530

    Create 103-KthLargestElementInAStream.js

---
+++ 103-KthLargestElementInAStream.js
@@ -0,0 +1,72 @@
+/**703. Kth Largest Element in a Stream
+Solved
+Easy
+Topics
+Companies
+
+Design a class to find the kth largest element in a stream. Note that it is the kth largest element in the sorted order, not the kth distinct element.
+
+Implement KthLargest class:
+
+    KthLargest(int k, int[] nums) Initializes the object with the integer k and the stream of integers nums.
+    int add(int val) Appends the integer val to the stream and returns the element representing the kth largest element in the stream.
+
+ 
+
+Example 1:
+
+Input
+["KthLargest", "add", "add", "add", "add", "add"]
+[[3, [4, 5, 8, 2]], [3], [5], [10], [9], [4]]
+Output
+[null, 4, 5, 5, 8, 8]
+
+Explanation
+KthLargest kthLargest = new KthLargest(3, [4, 5, 8, 2]);
+kthLargest.add(3);   // return 4
+kthLargest.add(5);   // return 5
+kthLargest.add(10);  // return 5
+kthLargest.add(9);   // return 8
+kthLargest.add(4);   // return 8
+
+ 
+
+Constraints:
+
+    1 <= k <= 104
+    0 <= nums.length <= 104
+    -104 <= nums[i] <= 104
+    -104 <= val <= 104
+    At most 104 calls will be made to add.
+    It is guaranteed that there will be at least k elements in the array when you search for the kth element.
+
+ */
+
+
+/**
+ * @param {number} k
+ * @param {number[]} nums
+ */
+var KthLargest = function(k, nums) {
+    this.main = new MinPriorityQueue();
+    for(let i = 0; i < nums.length; i++){
+        this.main.enqueue(nums[i]);
+    }
+    this.k = k;
+    while(this.main.size() > k) this.main.dequeue().element;
+};
+KthLargest.prototype.add = function(val) {
+    this.main.enqueue(val);
+    if(this.main.size() > this.k) this.main.dequeue().element;
+     return this.main.front().element;
+};
+/** 
+ * @param {number} val
+ * @return {number}
+ */
+
+/** 
+ * Your KthLargest object will be instantiated and called as such:
+ * var obj = new KthLargest(k, nums)
+ * var param_1 = obj.add(val)
+ */
