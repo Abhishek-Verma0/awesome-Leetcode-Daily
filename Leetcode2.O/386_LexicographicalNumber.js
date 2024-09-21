c85a605d9fb061cc994e93b6fe5fa34b19b99f1e
Abhishek-Verma0 <144248104+Abhishek-Verma0@users.noreply.github.com>
2024-09-21 12:47:18 +0530

    Create 386_LexicographicalNumber.js

---
+++ Leetcode2.O/386_LexicographicalNumber.js
@@ -0,0 +1,49 @@
+/**386. Lexicographical Numbers
+Medium
+Topics
+Companies
+
+Given an integer n, return all the numbers in the range [1, n] sorted in lexicographical order.
+
+You must write an algorithm that runs in O(n) time and uses O(1) extra space. 
+
+ 
+
+Example 1:
+
+Input: n = 13
+Output: [1,10,11,12,13,2,3,4,5,6,7,8,9]
+
+Example 2:
+
+Input: n = 2
+Output: [1,2]
+
+ 
+
+Constraints:
+
+    1 <= n <= 5 * 104
+
+ */
+
+
+/**
+ * @param {number} n
+ * @return {number[]}
+ */
+var lexicalOrder = function(n) {
+    let res=[]
+    function rec_adder (x,n){
+        if(x>n)return 
+        res.push(x)
+        let pe = x*10
+        for(let i=0;i<10;i++){
+            rec_adder(pe+i,n)
+        }
+    }
+    for(let i=1;i<10;i++){
+        rec_adder(i,n)
+    }
+    return res
+};
