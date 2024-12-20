4d1fdfaa8357b4a57869f8f6dde5c1eb317c1377
Abhishek-Verma0 <144248104+Abhishek-Verma0@users.noreply.github.com>
2024-12-20 23:02:41 +0530

    Create 2415_ReverseOddLevelsOfBinaryTree.cpp

---
+++ Leetcode2.O/2415_ReverseOddLevelsOfBinaryTree.cpp
@@ -0,0 +1,112 @@
+/***2415. Reverse Odd Levels of Binary Tree
+Solved
+Medium
+Topics
+Companies
+Hint
+
+Given the root of a perfect binary tree, reverse the node values at each odd level of the tree.
+
+    For example, suppose the node values at level 3 are [2,1,3,4,7,11,29,18], then it should become [18,29,11,7,4,3,1,2].
+
+Return the root of the reversed tree.
+
+A binary tree is perfect if all parent nodes have two children and all leaves are on the same level.
+
+The level of a node is the number of edges along the path between it and the root node.
+
+ 
+
+Example 1:
+
+Input: root = [2,3,5,8,13,21,34]
+Output: [2,5,3,8,13,21,34]
+Explanation: 
+The tree has only one odd level.
+The nodes at level 1 are 3, 5 respectively, which are reversed and become 5, 3.
+
+Example 2:
+
+Input: root = [7,13,11]
+Output: [7,11,13]
+Explanation: 
+The nodes at level 1 are 13, 11, which are reversed and become 11, 13.
+
+Example 3:
+
+Input: root = [0,1,2,0,0,0,0,1,1,1,1,2,2,2,2]
+Output: [0,2,1,0,0,0,0,2,2,2,2,1,1,1,1]
+Explanation: 
+The odd levels have non-zero values.
+The nodes at level 1 were 1, 2, and are 2, 1 after the reversal.
+The nodes at level 3 were 1, 1, 1, 1, 2, 2, 2, 2, and are 2, 2, 2, 2, 1, 1, 1, 1 after the reversal.
+
+ 
+
+Constraints:
+
+    The number of nodes in the tree is in the range [1, 214].
+    0 <= Node.val <= 105
+    root is a perfect binary tree.
+
+ */
+
+
+/***
+ * What is Complete Binary Tree? -- A tree whose every level is completely filled except the last level.
+ * A tree in which we fill element of last level from left to right
+ */
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* reverseOddLevels(TreeNode* root) {
+       // we will use BFS to solve this problem 
+       queue<TreeNode*> que;
+       que.push(root);
+       int lvl=0;
+
+       while(!que.empty()){
+        int n=que.size();
+
+        vector<TreeNode*> lvlNode;
+
+        while(n--){
+            TreeNode* temp=que.front();
+            que.pop();
+            lvlNode.push_back(temp);  
+
+            if(temp->left){
+                que.push(temp->left);
+            }
+            if(temp->right){
+                que.push(temp->right);
+            }
+        }
+        if(lvl%2==1){
+            int i=0;
+            int j= lvlNode.size() -1 ;
+            while(i<j){
+                int tmp=lvlNode[i]->val;
+                lvlNode[i]->val=lvlNode[j]->val;
+                lvlNode[j]->val=tmp;
+                i++;
+                j--;
+            }
+        }
+        lvl++;
+       } 
+       return root;
+    }
+};
