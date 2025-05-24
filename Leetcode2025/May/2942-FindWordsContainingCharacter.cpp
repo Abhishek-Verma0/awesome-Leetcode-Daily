63fad29c6be5b60671fed049e00e05f4de6cab31
Abhishek Verma <144248104+Abhishek-Verma0@users.noreply.github.com>
2025-05-25 03:40:13 +0530

    Second Approach Lamda Capture

--- Leetcode2025/May/2942-FindWordsContainingCharacter.cpp
+++ Leetcode2025/May/2942-FindWordsContainingCharacter.cpp
@@ -23,3 +23,28 @@ public:
         return res;
     }
 };
+
+
+//  Second Approach -- Using lamda capture and any_of() -- STL
+
+
+class Solution {
+public:
+    vector<int> findWordsContaining(vector<string>& words, char x) {
+        int n= words.size();
+        vector<int> res;
+
+        //  using lamda capture here syntax [] - for capture it capture var out of it's scope can use refrence or value ,, (params)-- it has parmaeter {body}; - remeber body always end woth semicolon(;)
+
+        auto  hasChar= [x](char c){
+            return c==x;
+        };
+        //  here we use any_of stl which returns true if any_char is matched
+        for(int i=0;i<n;i++){
+            if(any_of(begin(words[i]),end(words[i]),hasChar)==true){
+                res.push_back(i);
+            }
+        }
+return res;
+    }
+};
